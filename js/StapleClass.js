a4a72faec261fd03ecae6eb1791bb4e9dd50943c
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-12-20 21:29:10 +0800

    update: renamed a few functions in StapleClass.js

--- js/StapleClass.js
+++ js/StapleClass.js
@@ -1,21 +1,21 @@
 function gridColumnRowAutoLayout(){
     const potentialMatches = document.querySelectorAll('[class*="grid-"]');
     const gridRegex = /grid-(\d+)x(\d+)/;
-    Array.from(potentialMatches).reduce((item, el) => {
-        const gridClass = Array.from(el.classList).find(c => gridRegex.test(c));
+    Array.from(potentialMatches).reduce((item, element) => {
+        const gridClass = Array.from(element.classList).find(elementClass => gridRegex.test(elementClass));
         if (gridClass) {
             const match = gridClass.match(gridRegex);
             item.push({
-                element: el,
-                n1: parseInt(match[1], 10),
-                n2: parseInt(match[2], 10)
+                element: element,
+                columns: parseInt(match[1], 10),
+                rows: parseInt(match[2], 10)
             });
         }
         return item;
     }, []).forEach(item => {
         item.element.style.display = 'grid';
-        item.element.style.gridTemplateColumns = `repeat(${item.n1}, 1fr)`;
-        item.element.style.gridTemplateRows = `repeat(${item.n2}, 1fr)`;
+        item.element.style.gridTemplateColumns = `repeat(${item.columns}, 1fr)`;
+        item.element.style.gridTemplateRows = `repeat(${item.rows}, 1fr)`;
     });
 }
 
