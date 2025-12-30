bffbc56e8bf4755d6d7c932e828d28d920ad4785
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-12-30 12:52:59 +0800

    update: remove unused modules and classes to clean up the codebase

--- py/base64_to_string.py
+++
@@ -1,4 +0,0 @@
-import base64
-
-def base64_to_string(string):
-    return base64.b64decode(string.encode('utf-8')).decode('utf-8')
--- py/has_spaces.py
+++
@@ -1,8 +0,0 @@
-def has_spaces(string):
-    return any(a == " " for a in string)
-
-def has_spaces2(string):
-    for a in string:
-        if a == " ":
-            return True
-    return False
